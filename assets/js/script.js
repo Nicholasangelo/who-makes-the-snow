8325c70372896b390cc36045d3e97ba822cd131e
Nicholas Angelo <40876154+Nicholasangelo@users.noreply.github.com>
2018-12-03 23:50:42 -0500

    displaying up to page 14 with snokeflake transition

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,29 @@
-$(document).ready(function() {
-AOS.init();
+$(document).ready(function () {
+    AOS.init();
 
-$("#cover").hide();
-$("#img1").hide();
-$("#img2").hide();
-$(".oneEight").css("display", "none");
+    $("#cover").hide();
+    $("#img1").hide();
+    $("#img2").hide();
+    $(".oneEight").css("display", "none");
+    $(".nineTwel").css("display", "none");
+    $(".thirFour").css("display", "none");
+
 
 });
 function showAllPages() {
-    setTimeout(function() { 
+    setTimeout(function () {
         $("#cover").css("display", "none");
-    $("#img1").css("display", "none");;
-    $("#img2").css("display", "none");;
-    $("#clickGlobe").css("display", "none");
-    $(".oneEight").css("display", "block"); }, 12000);
+        $("#img1").css("display", "none");;
+        $("#img2").css("display", "none");;
+        $("#clickGlobe").css("display", "none");
+        $(".oneEight").css("display", "block");
+    }, 12000);
 }
 
-$("#clickGlobe").on("click", function(event) {
+$("#clickGlobe").on("click", function (event) {
     event.preventDefault();
     $("#cover").show();
-    $("#clickGlobe").on("click", function(event) {
+    $("#clickGlobe").on("click", function (event) {
         event.preventDefault();
         $("#cover").hide();
         $("#img1").show();
@@ -27,5 +31,21 @@ $("#clickGlobe").on("click", function(event) {
         $("#clickGlobe").hide();
         showAllPages();
     })
-  
+
 })
+
+$("#seven8btn").on("click", function (event) {
+    event.preventDefault();
+    $(".oneEight").css("display", "none");
+    $(".nineTwel").css("display", "block");
+    AOS.init();
+
+})
+
+$("#twelBtn").on("click", function (event) {
+    event.preventDefault();
+    $(".nineTwel").css("display", "none");
+    $(".thirFour").css("display", "block");
+    AOS.init();
+})
+
